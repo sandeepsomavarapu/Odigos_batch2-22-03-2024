f3fada0f3271f8f6f5ad7ce208dc0138c296d212
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2024-03-27 18:40:00 +0530

    27-03-2024

--- Variables_Ex.java
+++ Variables_Ex.java
@@ -1,8 +1,29 @@
 class Variables_Ex
 {
+	short marks=234;//global variable
 public static void main(String args[])
 {
 System.out.println("welcome to java");
+//local variables comments
+/*  
+variables two types
+local 
+global
+
+ */
+int salary=12345;
+float result=23.54391279126112f;//size 4 bytes
+double value=23.54391279126112;//
+long contact=9494949494l;
+char gender='M';
+boolean test=true;
+
+System.out.println(salary);
+System.out.println(result);
+System.out.println(value);
+System.out.println(contact);
+System.out.println(gender);
+System.out.println(test);
 }
 
 }
