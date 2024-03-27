e7d07e6d283e7f54c8e28493705ad44dda903ca7
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2024-03-27 07:57:59 +0530

    26-03-2024

---
+++ Variables_Ex.java
@@ -0,0 +1,16 @@
+class Variables_Ex
+{
+public static void main(String args[])
+{
+System.out.println("welcome to java");
+}
+
+}
+
+//jdk  compaliation-->JRE-->  JVM--0's 1's-->compiler
+
+//user lang-->machine lang
+//source code to byte code 
+//.java file-->.class
+
+//javac filename.java
